992986847e58fc41b1a14ffe00633745c708f8ef
PurinCompattana <jkshot000@gmail.com>
2022-08-18 22:38:11 +0700

    edit nav tab bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,15 @@
 
 @section('content')
 <style>
-    
+
 </style>
     <div class="container-fluid side">
         <nav class="nav nav-tabs">
-<<<<<<< HEAD
-            <a class="nav-link active" href="{{ route('home') }}">หน้าแรก</a>
-            <a class="nav-link" href="{{ route('employees.index') }}">จัดการพนักงาน</a>
-            <a class="nav-link" href="{{ route('equipments.index') }}">จัดการอุปกรณ์</a>
-            <a class="nav-link" href="#">คำร้อง</a>
-            <a class="nav-link" href="{{ route('withdraws.index') }}">เบิกอุปกรณ์</a>
-            <a class="nav-link" href="{{ route('withdraws.index') }}">เบิกอุปกรณ์</a>
-=======
             <a class="nav-link nav-link-color active" href="{{ route('home') }}">หน้าแรก</a>
             <a class="nav-link nav-link-color" href="{{ route('employees.index') }}">จัดการพนักงาน</a>
             <a class="nav-link nav-link-color" href="{{ route('equipments.index') }}">จัดการอุปกรณ์</a>
             <a class="nav-link nav-link-color" href="#">คำร้อง</a>
->>>>>>> 45a667f55cc95b2392ddb79973b1ee953a6b6d97
+            <a class="nav-link nav-link-color" href="{{ route('withdraws.index') }}">เบิกอุปกรณ์</a>
         </nav>
     </div>
 
--- resources/views/withdraw/index.blade.php
+++ resources/views/withdraw/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <div class="container-fluid side">
         <nav class="nav nav-tabs">
-            <a class="nav-link " href="{{ route('home') }}">หน้าแรก</a>
-            <a class="nav-link" href="{{ route('employees.index') }}">จัดการพนักงาน</a>
-            <a class="nav-link" href="{{ route('equipments.index') }}">จัดการอุปกรณ์</a>
-            <a class="nav-link" href="#">คำร้อง</a>
-            <a class="nav-link active" href="{{ route('withdraws.index') }}">เบิกอุปกรณ์</a>
+            <a class="nav-link nav-link-color" href="{{ route('home') }}">หน้าแรก</a>
+            <a class="nav-link nav-link-color" href="{{ route('employees.index') }}">จัดการพนักงาน</a>
+            <a class="nav-link nav-link-color" href="{{ route('equipments.index') }}">จัดการอุปกรณ์</a>
+            <a class="nav-link nav-link-color" href="#">คำร้อง</a>
+            <a class="nav-link nav-link-color active" href="{{ route('withdraws.index') }}">เบิกอุปกรณ์</a>
         </nav>
     </div>
 
