a0ee6597dc440083cd09316d03592279591dcf8c
PurinCompattana <jkshot000@gmail.com>
2022-09-16 10:43:25 +0700

    move location file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,21 +7,12 @@ use App\Models\Equipment;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $equipments = Equipment::query()->get();
